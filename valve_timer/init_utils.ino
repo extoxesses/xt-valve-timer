65bd7902bde5e2a8a12ccf078428d3a680445e44
extoxesses <extoxesses@gmail.com>
2022-06-13 21:42:55 +0200

    Completed lib conversion

---
+++ valve_timer/init_utils.ino
@@ -0,0 +1,54 @@
+void initDisplay(Adafruit_PCD8544& lcd, short contrast) {
+  lcd.begin();
+  lcd.setContrast(contrast);
+  lcd.clearDisplay();
+}
+
+void initGPIO() {
+  // Debug
+  pinMode(DEBUG_PIN, INPUT);
+  // Relay
+  for(short i = 0; i < MAX_VALVES; ++i) {
+    pinMode(RELAY_START_PIN, OUTPUT);
+    digitalWrite(RELAY_START_PIN + i, LOW);
+  }
+  // Interrupts
+  short mode = FALLING;
+  attachInterrupt(digitalPinToInterrupt(ENTER_BTN_PIN),  onEnterRise,  mode);
+  attachInterrupt(digitalPinToInterrupt(RETURN_BTN_PIN), onReturnRise, mode);
+  attachInterrupt(digitalPinToInterrupt(PREV_BTN_PIN),   onPrevRise,   mode);
+  attachInterrupt(digitalPinToInterrupt(NEXT_BTN_PIN),   onNextRise,   mode);
+}
+
+void initStateMachine(StateFunction*** stateMachine, short** stateMachineSize, short size) {
+  short smSize = 1 + size;
+  *stateMachine = new StateFunction*[smSize];
+  *stateMachineSize = new short[smSize];
+  
+  (*stateMachine)[0] = SYSTEM_CFG;
+  (*stateMachineSize)[0] = SYSTEM_CFG_SIZE;
+  for (int i = 1; i < smSize; ++i) {
+      (*stateMachine)[i] = VALVE_CFG;
+      (*stateMachineSize)[i] = VALVE_CFG_SIZE;
+  }
+
+  refresh = true;
+}
+
+void initRTC(RtcDS1302<ThreeWire>& rtc) {
+  Serial.println("Started RTC configurations...");
+  rtc.Begin();
+
+  if (!rtc.IsDateTimeValid()) {
+    RtcDateTime compiled(__DATE__, __TIME__);
+    Serial.print("Invalid date-time. Use compile time: ");
+    Serial.println(compiled);
+    rtc.SetDateTime(compiled);
+  }
+
+  rtc.SetIsWriteProtected(false);
+  rtc.SetIsRunning(true);
+
+  Serial.print("RTC configured: ");
+  Serial.println(rtc.GetDateTime());
+}
---
+++ valve_timer/src/xtv_valve.cpp
@@ -0,0 +1,77 @@
+#include "../include/xtv_valve.h"
+
+Valve::Valve() {
+  manual = true;
+  active = false;
+  timerHour = 12;
+  timerMinute = 30;
+  duration = 10;
+
+  days = new bool[WEEK_SIZE];
+  for (short i = 0; i < WEEK_SIZE; ++i) {
+    days[i] = false;
+  }
+}
+
+bool Valve::isManual() {
+  return manual;
+}
+
+void Valve::setManual(bool manual) {
+  this->manual = manual;
+  changed = true;
+}
+
+bool Valve::isActive() {
+  return active;
+}
+
+void Valve::setActive(bool active) {
+  this->active = active;
+  changed = true;
+}
+
+short Valve::getTimerHour() {
+  return timerHour;
+}
+
+void Valve::setTimerHour(short timerHour) {
+  this->timerHour = timerHour;
+}
+
+short Valve::getTimerMinute() {
+  return timerMinute;
+}
+
+void Valve::setTimerMinute(short timerMinute) {
+  this->timerMinute = timerMinute;
+  changed = true;
+}
+
+int Valve::getDuration() {
+  return duration;
+}
+
+void Valve::setDuration(int duration) {
+  this->duration = duration;
+  changed = true;
+}
+
+bool Valve::getDay(int idx) {
+  return (idx < WEEK_SIZE) ? days[idx] : false;
+}
+
+void Valve::setDay(int idx, bool state) {
+  if (idx < WEEK_SIZE) {
+    days[idx] = state;
+    changed = true;
+  }
+}
+
+bool Valve::isChanged() {
+  return changed;
+}
+
+void Valve::setChanged() {
+  this->changed = false;
+}
